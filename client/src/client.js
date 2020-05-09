6283238e519a53231aceeba21c47d125b550b5b9
Blessing Krofegha <bkrofegha@gmail.com>
2020-05-09 23:16:14 +0200

    add comment

--- client/src/client.js
+++ client/src/client.js
@@ -1,9 +1,10 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
-import { ApolloLink } from "apollo-link";  
-import { setContext } from 'apollo-link-context'
 
 import gql from 'graphql-tag'
 
-
+/**
+ * Create an Apollo Client, Link it to an API,
+ *  and export it as a default client to our Pet component.
+ */
