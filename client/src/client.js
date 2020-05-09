fac130a5c89e53993a37dd0c05799d1836260c48
Blessing Krofegha <bkrofegha@gmail.com>
2020-05-10 00:51:13 +0200

    correct uri

--- client/src/client.js
+++ client/src/client.js
@@ -2,9 +2,16 @@ import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 
-import gql from 'graphql-tag'
-
 /**
  * Create an Apollo Client, Link it to an API,
- *  and export it as a default client to our Pet component.
+ *  and export it as a default client
  */
+
+const link = new HttpLink({ uri: "http://localhost:4000/" });
+const cache = new InMemoryCache()
+const client = new ApolloClient({
+  link,
+  cache
+})
+
+export default client
